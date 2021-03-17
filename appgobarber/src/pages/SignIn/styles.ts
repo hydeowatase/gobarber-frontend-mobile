ee567c395aeee85a6deb0212418e68c8dd5a2a06
hydeowatase <hydeowatase@gmail.com>
2021-03-17 17:07:48 -0300

    Go-Barber Mobile: SignIn Route done.

--- appgobarber/src/pages/SignIn/styles.ts
+++ appgobarber/src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import style from 'styled-components/native';
+import {getBottomSpace} from 'react-native-iphone-x-helper';
 
 export const Container = style.View`
   flex: 1;
@@ -12,4 +13,37 @@ export const Title = style.Text`
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
   margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = style.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = style.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const CreateAccountButton = style.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CreateAccountButtonText = style.Text`
+  justify-content: center;
+  color: #ff9000;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
 `;
